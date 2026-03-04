cefaefc10612830e83df18f9e09121637e14da65
archita15 <53861050+archita15@users.noreply.github.com>
2026-03-04 10:16:41 -0500

    Added new changes

--- Frontend/src/utils/config.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-export const API_URL = "http://localhost:8081/api/v1"
-const instance = axios.create({
-    baseURL: "http://localhost:8081/api/v1",
-});
-
-export default instance;
